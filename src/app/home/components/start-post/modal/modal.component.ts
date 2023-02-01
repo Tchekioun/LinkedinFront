d72ea759483a349eab9362932075810144e1d2b0
Abderraouf <abdelraouffinch@gmail.com>
2023-02-01 23:49:26 +0100

    add modal function when posting and dismiss

--- src/app/home/components/start-post/modal/modal.component.ts
+++ src/app/home/components/start-post/modal/modal.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modal',
@@ -6,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modal.component.scss'],
 })
 export class ModalComponent implements OnInit {
+  @ViewChild('form') from!: NgForm;
 
-  constructor() { }
+  constructor(private modalController: ModalController) {}
 
   ngOnInit() {}
 
+  onDismiss() {
+    this.modalController.dismiss(null, 'dismiss from X button');
+  }
+
+  onPost() {
+    if (!this.from.valid) return;
+    const body = this.from.value.body;
+    this.modalController.dismiss(
+      {
+        post: {
+          body,
+          createdAt: new Date(),
+        },
+      },
+      'post'
+    );
+  }
 }
