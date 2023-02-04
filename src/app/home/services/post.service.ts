be57af3a1a1caf051493d174ef99528ac6dca616
Abderraouf <abdelraouffinch@gmail.com>
2023-02-04 22:54:33 +0100

    Add  post service

--- src/app/home/services/post.service.ts
+++ src/app/home/services/post.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from '../models/post';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  httpUrl = 'http://localhost:3000';
+  getSelectedPosts(params: any): Observable<Post[]> {
+    return this.http.get<Post[]>(`${this.httpUrl}/feeds${params}`);
+  }
 }
