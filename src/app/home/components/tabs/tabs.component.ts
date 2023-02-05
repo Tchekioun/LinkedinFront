a869a9b74e88777ba6940901f190831edeb53c5e
Abderraouf <abdelraouffinch@gmail.com>
2023-02-05 21:57:08 +0100

    FEAT: add tab header

--- src/app/home/components/tabs/tabs.component.ts
+++ src/app/home/components/tabs/tabs.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ModalComponent } from '../start-post/modal/modal.component';
 
 @Component({
   selector: 'app-tabs',
@@ -6,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.component.scss'],
 })
 export class TabsComponent implements OnInit {
-
-  constructor() { }
+  constructor(public modalController: ModalController) {}
 
   ngOnInit() {}
 
+  async presentModal() {
+    const modal = await this.modalController.create({
+      component: ModalComponent,
+      cssClass: 'my-custom-class2',
+    });
+    await modal.present();
+    const { role } = await modal.onDidDismiss();
+    console.log(7, role);
+  }
 }
